738fd1c74302e4d17465887b74cd6a83b700f5f4
Andre <andre.roussel@ccnb.ca>
2021-03-25 15:31:51 -0300

    Basic helper files trial

---
+++ OLED_Basic.h
@@ -0,0 +1,43 @@
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET -1
+Adafruit_SSD1306 display(OLED_RESET);
+
+
+void oledBegin()
+{
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
+  display.clearDisplay();
+}
+
+void basicPrintOLED(String text, float value, String units)
+{
+  
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println(text);
+  display.setTextSize(3);
+  display.setCursor(0,8);
+  display.print(value);
+  display.print(" " + units);
+  display.display();
+
+  delay(200);
+}
+
+void basicPrintOK()
+{
+  
+  display.clearDisplay();
+  
+  display.setTextSize(3);
+  display.setCursor(0,8);
+  display.print("OK");
+  
+  display.display();
+
+  delay(4000);
+}
